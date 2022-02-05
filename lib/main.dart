bda4c7c0e37d3ad1ef7839c7d6dc7b962f846348
Abdurrhamn <88352871+Abdurrhamn@users.noreply.github.com>
2022-02-05 15:11:21 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Wa_me_interface());
+
+class Wa_me_interface extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
